e447d2679d66f6283f5afc3dccf9d37df24667db
ksChhetri <kschhetri1@gmail.com>
2018-04-13 16:33:20 +0530

    alert invalid address

--- app.js
+++ app.js
@@ -20,6 +20,15 @@ function createContract() {
        alert("please enter at least 2 addresses");
        return;
     }
+    else{
+      if(!isAddress(ca1)){
+        alert('address one is invalid format');
+      }
+
+      if(!isAddress(ca2)){
+        alert('address second is invalid format');
+      }
+    }
 
     if (!myAddress) {
         alert("unlock metamask!");
@@ -133,35 +142,35 @@ var balance = web3.eth.accounts.sign("0x4b7e34384b6656ccbb8a99594a599cf4dbe6350d
         console.error(error);
 });
 }
+var isAddress = function (address) {
+    if (!/^(0x)?[0-9a-f]{40}$/i.test(address)) {
+        // check if it has the basic requirements of an address
+        return false;
+    } else if (/^(0x)?[0-9a-f]{40}$/.test(address) || /^(0x)?[0-9A-F]{40}$/.test(address)) {
+        // If it's all small caps or all all caps, return true
+        return true;
+    } else {
+        // Otherwise check each case
+        return isChecksumAddress(address);
+    }
+};
 
-// function getAccounts() {
-// 	web3.eth.getAccounts(function(err, accounts) {
-// 		if (err) {
-// 			$(".loading-container").hide();
-// 			showAlert(err, err.message);
-// 			return;
-// 		}
-
-// 		console.log(accounts);
-// 	});
-// }
-
-// function generateAddress() {
-//     var params = { keyBytes: 32, ivBytes: 16 };
-
-//     var dk = keythereum.create(params);
-
-//     keythereum.create(params, function (dk) {
-//       var options = {};
-//       var password = generatePassword();
-//       keythereum.dump(password, dk.privateKey, dk.salt, dk.iv, options, function (keyObject) {
-//         console.log(keyObject);
-//         console.log(JSON.stringify(keyObject));
-//         console.log(keyObject, password,"****");
-//       });
-//     });
-//   }
-
-//   function generatePassword() {
-//     return passwordGenerator(20, false);
-//   }
+/**
+ * Checks if the given string is a checksummed address
+ *
+ * @method isChecksumAddress
+ * @param {String} address the given HEX adress
+ * @return {Boolean}
+*/
+var isChecksumAddress = function (address) {
+    // Check each case
+    address = address.replace('0x','');
+    var addressHash = sha3(address.toLowerCase());
+    for (var i = 0; i < 40; i++ ) {
+        // the nth letter should be uppercase if the nth digit of casemap is 1
+        if ((parseInt(addressHash[i], 16) > 7 && address[i].toUpperCase() !== address[i]) || (parseInt(addressHash[i], 16) <= 7 && address[i].toLowerCase() !== address[i])) {
+            return false;
+        }
+    }
+    return true;
+};
